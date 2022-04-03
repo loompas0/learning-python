ec8dd4bf3ecfa6d674d4cd72521e86e10ca151c8
loompas0 <remybernheim@yahoo.com>
2022-04-03 12:24:27 +0200

    some training ahead , all debug

---
+++ main.py
@@ -0,0 +1,26 @@
+
+# 
+your_Name = ""
+age_Str = ""
+age_Num = 0
+
+while your_Name == "":
+    your_Name = input ("Quel est votre nom ? ")
+    if your_Name =="":
+        print ("Erreur Votre nom ne peut être vide")
+
+while age_Num <= 0:
+    age_Str  = (input ("Quel age avez vous ? "))
+    try:
+        age_Num = int (age_Str) 
+    except:
+        print ("Erreur : vous devez entrer un nombre positif pour l'age , pas une chaine de caractère")
+    else:
+        age_Num = int (age_Str)
+        if age_Num <= 0:
+            print ("Erreur : vous devez entrer un nombre positif pour l'age , pas  négatif")
+
+print("Vous vous appelez " + your_Name + ".")
+print ("Vous avez " + str(age_Num) + " ans. ")
+print ("L'an procahain vous aurez " + str(age_Num+ 1) + " ans. ")
+
